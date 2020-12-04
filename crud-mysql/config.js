23f5971478dd787974e50a25ed658f5eea444652
Jon Liu <mr@sianliu.com>
2020-12-04 17:57:52 +0800

    first commit crud app

---
+++ crud-mysql/config.js
@@ -0,0 +1,15 @@
+var config = {
+    database: {
+        host:      'terraform-20201204052419839700000001.cds0tbubnduj.ap-southeast-1.rds.amazonaws.com', 
+        user:       'master',         
+        password: '6Fa0xda4fmg4',         
+        port:       3306,         
+        db:       'test'         
+    },
+    server: {
+        host: '127.0.0.1',
+        port: '3000'
+    }
+}
+ 
+module.exports = config
